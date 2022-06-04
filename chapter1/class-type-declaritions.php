674fbfc65a1212e415c10b583e637eb90b808b95
= <anton.graham.2011@gmail.com>
2022-06-03 23:20:59 -0400

    update class-type-declaritions cheat sheet

--- chapter1/class-type-declaritions.php
+++ chapter1/class-type-declaritions.php
@@ -1,24 +1,24 @@
 
 <?php
 
-require_once 'Playlist.php';
-require_once 'Song.php';
+require_once 'Playlist.php'; //require class Playlist
+require_once 'Song.php'; //require class Song
 
 
-$playlist = new Playlist();
+$playlist = new Playlist(); //create a new playlist object
 
-$song1 = new Song('Oh Happy Days',2);
-$song2 = new Song('I love You',3);
+$song1 = new Song('Oh Happy Days',2); // create a new song object
+$song2 = new Song('I love You',3); // create a new song object
 
-$playlist->addSong($song1);
-$playlist->addSong($song2);
+$playlist->addSong($song1); // call playlist method
+$playlist->addSong($song2); // call playlist method
 
-print count($playlist->songs);
+print count($playlist->songs); //this is an array see Playlist.php
 
-foreach($playlist->songs as $song)
+foreach($playlist->songs as $song)  //for every array element
 {
-  print $song->name .PHP_EOL;
-  print $song->numberOfPlays.PHP_EOL;
+  print $song->name .PHP_EOL; // get song property name
+  print $song->numberOfPlays.PHP_EOL; // get song property numberOfPlays
 }
 
 
